7362714509da706052a8ec4a51c2c4f31a4886f3
Serdar Çoruhlu <serdar.coruhlu@hotmail.com>
2025-04-01 00:14:29 +0300

    clarify the example

--- entry.h
+++ entry.h
@@ -1,27 +1,11 @@
 #ifndef __ENTRY_H__
 #define __ENTRY_H__
 
-/*
-Entry lang
->
-<
-add
-dec
-print
-ftext
-if
-nfi
-rev
-*/
-
 #include <stddef.h>
 #include <stdint.h>
 #include <stdbool.h>
 #include <string.h>
-#define SKIP \
-if (entry.skip) {\
-    entry.skip = false;\
-} else\
+#include <stdio.h>
 
 typedef struct {
     int32_t sandwich[256];
@@ -42,13 +26,18 @@ HEADACHE go(char* fs);
 
 #ifdef ENTRY_IMPLEMENTATION
 
-// fs is assumed to be null-terminated
-HEADACHE go(char* fs) {
+#define __SKIP \
+if (entry.skip) {\
+    entry.skip = false;\
+} else\
+
+// code is assumed to be null-terminated
+HEADACHE go(char* code) {
     Entry entry = {0};
-    while (fs && *fs) {
+    while (code && *code) {
         char buffer[6] = {0};
         for (int i = 0; i < 5; ++i) {
-            char c = *(fs + i);
+            char c = *(code + i);
             if (c) {
                 buffer[i] = c;
             } else {
@@ -57,56 +46,66 @@ HEADACHE go(char* fs) {
             if (i == 0) {
                 // > and <
                 if (buffer[0] == '>') {
-                    SKIP {
-                        entry.ps++;
-                        if (entry.ps >= 256) entry.ps = 0;
+                    __SKIP {
+                        if (!entry.direction) {
+                            entry.ps++;
+                            if (entry.ps >= 256) entry.ps = 0;
+                        } else {
+                            if (entry.ps == 0) entry.ps = 256;
+                            entry.ps--;
+                        }
                     }
                     break;
                 } else if (buffer[0] == '<') {
-                    SKIP {
-                        if (entry.ps == 0) entry.ps = 256;
-                        entry.ps--;
+                    __SKIP {
+                        if (entry.direction) {
+                            entry.ps++;
+                            if (entry.ps >= 256) entry.ps = 0;
+                        } else {
+                            if (entry.ps == 0) entry.ps = 256;
+                            entry.ps--;
+                        }
                     }
                     break;
                 }
             } else if (i == 1) {
                 // if
                 if (!strcmp(buffer, "if")) {
-                    SKIP {
-                        entry.skip = !(entry.sandwich[entry.ps] == 0);
+                    __SKIP {
+                        entry.skip = (entry.sandwich[entry.ps] > 0);
                     }
                     break;
                 }
             } else if (i == 2) {
                 // add, dec and rev
                 if (!strcmp(buffer, "add")) {
-                    SKIP {
+                    __SKIP {
                         entry.sandwich[entry.ps]++;
                     }
                     break;
                     
                 } else if (!strcmp(buffer, "dec")) {
-                    SKIP {
+                    __SKIP {
                         entry.sandwich[entry.ps]--;
                     }
                     break;
                     
                 } else if(!strcmp(buffer, "rev")) {
-                    SKIP {
+                    __SKIP {
                         entry.direction = !entry.direction;
                     }
                     break;
                     
                 } else if(!strcmp(buffer, "nfi")) {
-                    SKIP {
-                        entry.skip = (entry.sandwich[entry.ps] == 0);
+                    __SKIP {
+                        entry.skip = (entry.sandwich[entry.ps] <= 0);
                     }
                     break;
                 }
             } else if (i == 4) {
                 // print and ftext
                 if (!strcmp(buffer, "print")) {
-                    SKIP {
+                    __SKIP {
                         int32_t i = entry.sandwich[entry.ps];
                         if (i >= 256) {
                             return WTF;
@@ -116,7 +115,7 @@ HEADACHE go(char* fs) {
                     break;
                     
                 } else if (!strcmp(buffer, "ftext")) {
-                    SKIP {
+                    __SKIP {
                         entry.sandwich[entry.ps] = fgetc(stdin);
                     }
                     break;
@@ -124,9 +123,9 @@ HEADACHE go(char* fs) {
             }
         }
         if (entry.direction) {
-            --fs;
+            --code;
         } else {
-            ++fs;
+            ++code;
         }
     }
 
--- example.c
+++ example.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #define ENTRY_IMPLEMENTATION
 #include "entry.h"
 
 int main(void) {
-    char* code = "ftext if rev dec nfi rev add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add print > add  add  add  add  add  add  add  add  add  add print";
+    char* code =
+        "ftext print" // Set current cell to user input and echo it
+        "<add add add add add add add add add add print>" // Print newline
+        "if rev dec nfi rev" // Decrement current cell until it is <= zero
+        ">" // Switch to new 0 cell
+        "add add add add add add add add add" // Set current cell to 9
+        "ifrevdec> add add add add add add add add <nfirev" // Add 8 to the adjacent cell every iteration to set it to 8*9 = 72
+        "> print" // Switch to the cell with the value of 72 and print it
+        "<<<print" // Print newline
+    ;
     switch (go(code)) {
     case WHEREISTHEHOLE:
         printf("[RUNTIME ERROR] WHEREISTHEHOLE\n");
