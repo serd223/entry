6719fdeae4ddee114f2a1dd9e7bc7cf658ba6f80
Serdar Çoruhlu <serdar.coruhlu@hotmail.com>
2025-03-31 22:38:46 +0300

    add newline to print in example

--- example.c
+++ example.c
@@ -3,7 +3,7 @@
 #include "entry.h"
 
 int main(void) {
-    char* code = "ftext if rev dec nfi rev add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add print";
+    char* code = "ftext if rev dec nfi rev add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add  add print > add  add  add  add  add  add  add  add  add  add print";
     go(code);
     return 0;
 }
